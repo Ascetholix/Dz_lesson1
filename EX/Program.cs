263dd588f1a8e8a460ef2dcab8f58a212499515f
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-02 18:19:28 +0300

    Исправление задание 11

--- EX/Program.cs
+++ EX/Program.cs
@@ -1,23 +1,23 @@
-﻿//11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+﻿//11. Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
 //456 -> 46
 //782 -> 72
 //918 -> 98
 
-//Console.Write("Введите  число ");
+
 int n = new Random().Next(100, 1000);
 Console.WriteLine(n);
 
-int num1 = n / 10;
+int div = n / 10;
 
-int num2 = num1 % 10;
+int modDiv = div % 10;
 
-int num3 = n % 10;
+int modN = n % 10;
 
-int result = num1 - num2 + num3;
+int result = div - modDiv + modN;
 
 
-Console.WriteLine(num1);
-Console.WriteLine(num2);
-Console.WriteLine(num3);
-Console.WriteLine(result);
-
+Console.WriteLine($"Деление {div}");
+Console.WriteLine($"Остаток {modDiv}");
+Console.WriteLine($"Остаток {modN}");
+Console.WriteLine($"Результат {result}");
