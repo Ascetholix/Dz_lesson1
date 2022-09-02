115d556707528547e4b9d9c46d6f135a03e6af04
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-02 18:07:44 +0300

    ad ex4

---
+++ EX4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напиши программу, которая по введённому возрасту пользователя сообщает, 
+//к какой возрастной группе он относится:
+//до 13 включительно – детство;
+//от 14 до 24 – молодость;
+//от 25 до 59 – зрелость;
+//от 60 – старость.
+
+Console.Write("Ведите возраст : ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age > 0 && age <= 13) Console.WriteLine($"{age} лет. Детский возраст");
+if (age >= 14 && age <= 24) Console.WriteLine($"{age} лет. Молодой возраст");
+if (age >= 25 && age <= 59) Console.WriteLine($"{age} лет. Зрелый возраст");
+if (age > 60) Console.WriteLine($"{age} лет. Старость");
+//Console.WriteLine(age);
